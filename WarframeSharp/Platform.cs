49eebbe5901f44b7c8878b8d22b4eb29956294b5
Raxdiam <raxdiam@users.noreply.github.com>
2019-04-04 14:51:48 -0400

    Platform properties to readonly

--- WarframeSharp/Platform.cs
+++ WarframeSharp/Platform.cs
@@ -9,13 +9,13 @@
             _value = value;
         }
 
-        public static Platform Pc = new Platform("/pc");
+        public static readonly Platform Pc = new Platform("/pc");
 
-        public static Platform Ps4 = new Platform("/ps4");
+        public static readonly Platform Ps4 = new Platform("/ps4");
 
-        public static Platform XboxOne = new Platform("/xb1");
+        public static readonly Platform XboxOne = new Platform("/xb1");
 
-        public static Platform Switch = new Platform("/swi");
+        public static readonly Platform Switch = new Platform("/swi");
 
         public static implicit operator string(Platform platform) => platform._value;
     }
